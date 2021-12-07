46400e079325c6cf0ef1fb701fce14dd609e1b81
libos <benson60703@gmail.com>
2021-12-07 21:41:38 +0800

    Add: basic mathematics function Nodes & tests     negative, sin, cos, tan, exp, log, sqrt, abs

--- autodiff.hpp
+++ autodiff.hpp
@@ -1,144 +1,279 @@
 #pragma once
 
 #include <memory>
+#include <cmath>
 
+namespace autodiff{
 
-struct Node{
-    double value;
+    struct Node{
+        double value;
 
-    Node(const double &v): value(v) {}
-    ~Node() {}
-    virtual void prop(const double &output) = 0;
-};
+        Node(const double &v): value(v) {}
+        ~Node() {}
+        virtual void prop(const double &output) = 0;
+    };
 
-struct ConstantNode: Node{
-    ConstantNode(const double &v) : Node(v) {}
-    void prop(const double & /*output*/) override { /* do nothing */ }
-};
+    struct ConstantNode: Node{
+        ConstantNode(const double &v) : Node(v) {}
+        void prop(const double & /*output*/) override { /* do nothing */ }
+    };
 
-struct VarNode : Node{
-    double grad;
+    struct VarNode : Node{
+        double grad;
 
-    VarNode(const double &v): Node(v), grad(0.0) {}
-};
+        VarNode(const double &v): Node(v), grad(0.0) {}
+    };
 
-struct IndVarNode: VarNode{
-    IndVarNode(const double &v): VarNode(v) {}
-    void prop(const double &output) override{
-        grad += output;
+    struct IndVarNode: VarNode{
+        IndVarNode(const double &v): VarNode(v) {}
+        void prop(const double &output) override{
+            grad += output;
+        }
+    };
+
+    struct BinaryOpNode: Node{
+        std::shared_ptr<Node> left, right;
+
+        BinaryOpNode(const double &v,
+            const std::shared_ptr<Node> &l,
+            const std::shared_ptr<Node> &r) :
+            Node(v),
+            left(l),
+            right(r)
+            {}
+    };
+
+    struct AddOpNode: BinaryOpNode{
+        AddOpNode(const double &v,
+            const std::shared_ptr<Node> &l,
+            const std::shared_ptr<Node> &r) :
+            BinaryOpNode(v, l, r)
+            {}
+
+        void prop(const double &output) override{
+            left->prop(output);
+            right->prop(output);
+        }
+    };
+
+    struct SubOpNode: BinaryOpNode{
+        SubOpNode(const double &v,
+            const std::shared_ptr<Node> &l,
+            const std::shared_ptr<Node> &r) :
+            BinaryOpNode(v, l, r)
+            {}
+
+        void prop(const double &output) override{
+            left->prop(output);
+            right->prop(-output);
+        }
+    };
+
+    struct MulOpNode: BinaryOpNode{
+        MulOpNode(const double &v,
+            const std::shared_ptr<Node> &l,
+            const std::shared_ptr<Node> &r) :
+            BinaryOpNode(v, l, r)
+            {}
+
+        void prop(const double &output) override{
+            left->prop(right->value * output);
+            right->prop(left->value * output);
+        }
+    };
+
+    struct DivOpNode: BinaryOpNode{
+        DivOpNode(const double &v,
+            const std::shared_ptr<Node> &l,
+            const std::shared_ptr<Node> &r) :
+            BinaryOpNode(v, l, r)
+            {}
+
+        void prop(const double &output) override{
+            double recRight = 1.0 / right->value;
+            left->prop(recRight * output);
+            right->prop(recRight * recRight * (-left->value) * output);
+        }
+    };
+
+    struct UnaryOpNode: Node{
+        std::shared_ptr<Node> m;
+
+        UnaryOpNode(const double &v,
+            const std::shared_ptr<Node> m) :
+            Node(v),
+            m(m)
+            {}
+    };
+
+    struct NegOpNode: UnaryOpNode{
+        NegOpNode(const double &v,
+            const std::shared_ptr<Node> m) :
+            UnaryOpNode(v, m)
+            {}
+        void prop(const double &output) override{
+            m->prop(-output);
+        }
+    };
+
+    struct SinOpNode: UnaryOpNode{
+        SinOpNode(const double &v,
+            const std::shared_ptr<Node> m) :
+            UnaryOpNode(v, m)
+            {}
+        void prop(const double &output) override{
+            m->prop(std::cos(m->value) * output);
+        }
+    };
+
+    struct CosOpNode: UnaryOpNode{
+        CosOpNode(const double &v,
+            const std::shared_ptr<Node> m) :
+            UnaryOpNode(v, m)
+            {}
+        void prop(const double &output) override{
+            m->prop(std::sin(m->value) * (-output));
+        }
+    };
+
+    struct TanOpNode: UnaryOpNode{
+        TanOpNode(const double &v,
+            const std::shared_ptr<Node> m) :
+            UnaryOpNode(v, m)
+            {}
+        void prop(const double &output) override{
+            double secx = 1.0 / std::cos(m->value);
+            m->prop(secx * secx * output);
+        }
+    };
+
+    struct ExpOpNode: UnaryOpNode{
+        ExpOpNode(const double &v,
+            const std::shared_ptr<Node> m) :
+            UnaryOpNode(v, m)
+            {}
+        void prop(const double &output) override{
+            m->prop(std::exp(m->value) * output);
+        }
+    };
+
+    struct LogOpNode: UnaryOpNode{
+        LogOpNode(const double &v,
+            const std::shared_ptr<Node> m) :
+            UnaryOpNode(v, m)
+            {}
+        void prop(const double &output) override{
+            m->prop(output / m->value);
+        }
+    };
+
+    struct SqrtOpNode: UnaryOpNode{
+        SqrtOpNode(const double &v,
+            const std::shared_ptr<Node> m) :
+            UnaryOpNode(v, m)
+            {}
+        void prop(const double &output) override{
+            m->prop(output / (2.0 * std::sqrt(m->value)));
+        }
+    };
+
+    struct AbsOpNode: UnaryOpNode{
+        AbsOpNode(const double &v,
+            const std::shared_ptr<Node> m) :
+            UnaryOpNode(v, m)
+            {}
+        void prop(const double &output) override{
+            if(m->value > 0.0){
+                m->prop(output);
+            }else if (m->value < 0.0){
+                m->prop(-output);
+            }else{
+                m->prop(0.0);
+            }
+        }
+    };
+
+    std::shared_ptr<Node> operator+(const std::shared_ptr<Node> &l, const std::shared_ptr<Node> &r) {
+        return std::make_shared<AddOpNode>(l->value + r->value, l, r);
     }
-};
 
-struct BinaryOpNode: Node{
-    std::shared_ptr<Node> left, right;
-
-    BinaryOpNode(const double &v,
-        const std::shared_ptr<Node> &l,
-        const std::shared_ptr<Node> &r) :
-        Node(v),
-        left(l),
-        right(r)
-        {}
-};
-
-struct AddOpNode: BinaryOpNode{
-    AddOpNode(const double &v,
-        const std::shared_ptr<Node> &l,
-        const std::shared_ptr<Node> &r) :
-        BinaryOpNode(v, l, r)
-        {}
-
-    void prop(const double &output) override{
-        left->prop(output);
-        right->prop(output);
+    std::shared_ptr<Node> operator+(const std::shared_ptr<Node> &l, const double &r) {
+        return std::make_shared<AddOpNode>(l->value + r, l, std::make_shared<ConstantNode>(r));
     }
-};
 
-struct SubOpNode: BinaryOpNode{
-    SubOpNode(const double &v,
-        const std::shared_ptr<Node> &l,
-        const std::shared_ptr<Node> &r) :
-        BinaryOpNode(v, l, r)
-        {}
-
-    void prop(const double &output) override{
-        left->prop(output);
-        right->prop(-output);
+    std::shared_ptr<Node> operator+(const double &l, const std::shared_ptr<Node> &r) {
+        return std::make_shared<AddOpNode>(l + r->value, std::make_shared<ConstantNode>(l), r);
     }
-};
 
-struct MulOpNode: BinaryOpNode{
-    MulOpNode(const double &v,
-        const std::shared_ptr<Node> &l,
-        const std::shared_ptr<Node> &r) :
-        BinaryOpNode(v, l, r)
-        {}
-
-    void prop(const double &output) override{
-        left->prop(right->value * output);
-        right->prop(left->value * output);
+    std::shared_ptr<Node> operator+(const std::shared_ptr<Node> &l) {
+        return l;
     }
-};
 
-struct DivOpNode: BinaryOpNode{
-    DivOpNode(const double &v,
-        const std::shared_ptr<Node> &l,
-        const std::shared_ptr<Node> &r) :
-        BinaryOpNode(v, l, r)
-        {}
-
-    void prop(const double &output) override{
-        const auto recRight = 1.0 / right->value;
-        left->prop(recRight * output);
-        right->prop(recRight * recRight * (-left->value) * output);
+    std::shared_ptr<Node> operator-(const std::shared_ptr<Node> &l, const std::shared_ptr<Node> &r) {
+        return std::make_shared<SubOpNode>(l->value - r->value, l, r);
     }
-};
 
-std::shared_ptr<Node> operator+(const std::shared_ptr<Node> &l, const std::shared_ptr<Node> &r) {
-    return std::make_shared<AddOpNode>(l->value + r->value, l, r);
+    std::shared_ptr<Node> operator-(const std::shared_ptr<Node> &l, const double &r) {
+        return std::make_shared<SubOpNode>(l->value - r, l, std::make_shared<ConstantNode>(r));
+    }
+
+    std::shared_ptr<Node> operator-(const double &l, const std::shared_ptr<Node> &r) {
+        return std::make_shared<SubOpNode>(l - r->value, std::make_shared<ConstantNode>(l), r);
+    }
+
+    std::shared_ptr<Node> operator-(const std::shared_ptr<Node> &l) {
+        return std::make_shared<NegOpNode>(-l->value, l);
+    }
+
+    std::shared_ptr<Node> operator*(const std::shared_ptr<Node> &l, const std::shared_ptr<Node> &r) {
+        return std::make_shared<MulOpNode>(l->value * r->value, l, r);
+    }
+
+    std::shared_ptr<Node> operator*(const std::shared_ptr<Node> &l, const double &r) {
+        return std::make_shared<MulOpNode>(l->value * r, l, std::make_shared<ConstantNode>(r));
+    }
+
+    std::shared_ptr<Node> operator*(const double &l, const std::shared_ptr<Node> &r) {
+        return std::make_shared<MulOpNode>(l * r->value, std::make_shared<ConstantNode>(l), r);
+    }
+
+    std::shared_ptr<Node> operator/(const std::shared_ptr<Node> &l, const std::shared_ptr<Node> &r) {
+        return std::make_shared<DivOpNode>(l->value / r->value, l, r);
+    }
+
+    std::shared_ptr<Node> operator/(const std::shared_ptr<Node> &l, const double &r) {
+        return std::make_shared<DivOpNode>(l->value / r, l, std::make_shared<ConstantNode>(r));
+    }
+
+    std::shared_ptr<Node> operator/(const double &l, const std::shared_ptr<Node> &r) {
+        return std::make_shared<DivOpNode>(l / r->value, std::make_shared<ConstantNode>(l), r);
+    }
+
+    std::shared_ptr<Node> sin(const std::shared_ptr<Node> &l){
+        return std::make_shared<SinOpNode>(std::sin(l->value), l);
+    }
+
+    std::shared_ptr<Node> cos(const std::shared_ptr<Node> &l){
+        return std::make_shared<CosOpNode>(std::cos(l->value), l);
+    }
+
+    std::shared_ptr<Node> tan(const std::shared_ptr<Node> &l){
+        return std::make_shared<TanOpNode>(std::tan(l->value), l);
+    }
+
+    std::shared_ptr<Node> exp(const std::shared_ptr<Node> &l){
+        return std::make_shared<ExpOpNode>(std::exp(l->value), l);
+    }
+
+    std::shared_ptr<Node> log(const std::shared_ptr<Node> &l){
+        return std::make_shared<LogOpNode>(std::log(l->value), l);
+    }
+
+    std::shared_ptr<Node> sqrt(const std::shared_ptr<Node> &l){
+        return std::make_shared<SqrtOpNode>(std::sqrt(l->value), l);
+    }
+
+    std::shared_ptr<Node> abs(const std::shared_ptr<Node> &l){
+        return std::make_shared<AbsOpNode>(std::abs(l->value), l);
+    }
 }
-
-std::shared_ptr<Node> operator+(const std::shared_ptr<Node> &l, const double &r) {
-    return std::make_shared<AddOpNode>(l->value + r, l, std::make_shared<ConstantNode>(r));
-}
-
-std::shared_ptr<Node> operator+(const double &l, const std::shared_ptr<Node> &r) {
-    return std::make_shared<AddOpNode>(l + r->value, std::make_shared<ConstantNode>(l), r);
-}
-
-std::shared_ptr<Node> operator-(const std::shared_ptr<Node> &l, const std::shared_ptr<Node> &r) {
-    return std::make_shared<SubOpNode>(l->value - r->value, l, r);
-}
-
-std::shared_ptr<Node> operator-(const std::shared_ptr<Node> &l, const double &r) {
-    return std::make_shared<SubOpNode>(l->value - r, l, std::make_shared<ConstantNode>(r));
-}
-
-std::shared_ptr<Node> operator-(const double &l, const std::shared_ptr<Node> &r) {
-    return std::make_shared<SubOpNode>(l - r->value, std::make_shared<ConstantNode>(l), r);
-}
-
-std::shared_ptr<Node> operator*(const std::shared_ptr<Node> &l, const std::shared_ptr<Node> &r) {
-    return std::make_shared<MulOpNode>(l->value * r->value, l, r);
-}
-
-std::shared_ptr<Node> operator*(const std::shared_ptr<Node> &l, const double &r) {
-    return std::make_shared<MulOpNode>(l->value * r, l, std::make_shared<ConstantNode>(r));
-}
-
-std::shared_ptr<Node> operator*(const double &l, const std::shared_ptr<Node> &r) {
-    return std::make_shared<MulOpNode>(l * r->value, std::make_shared<ConstantNode>(l), r);
-}
-
-std::shared_ptr<Node> operator/(const std::shared_ptr<Node> &l, const std::shared_ptr<Node> &r) {
-    return std::make_shared<DivOpNode>(l->value / r->value, l, r);
-}
-
-std::shared_ptr<Node> operator/(const std::shared_ptr<Node> &l, const double &r) {
-    return std::make_shared<DivOpNode>(l->value / r, l, std::make_shared<ConstantNode>(r));
-}
-
-std::shared_ptr<Node> operator/(const double &l, const std::shared_ptr<Node> &r) {
-    return std::make_shared<DivOpNode>(l / r->value, std::make_shared<ConstantNode>(l), r);
-}
-
--- test_autodiff.cpp
+++ test_autodiff.cpp
@@ -1,14 +1,16 @@
 #include <gtest/gtest.h>
 #include "autodiff.hpp"
 
+using namespace autodiff;
+
 TEST(AutoDiffTest, AddOpNodeTest) {
   auto a = std::make_shared<IndVarNode>(1.0);
   auto b = std::make_shared<IndVarNode>(2.0);
   auto a_b_add = a + b;
   a_b_add->prop(1.0);
-  EXPECT_EQ(a_b_add->value, 3.0);
-  EXPECT_EQ(a->grad, 1.0);
-  EXPECT_EQ(b->grad, 1.0);
+  EXPECT_NEAR(a_b_add->value, 3.0, 1e-10);
+  EXPECT_NEAR(a->grad, 1.0, 1e-10);
+  EXPECT_NEAR(b->grad, 1.0, 1e-10);
 }
 
 TEST(AutoDiffTest, SubOpNodeTest) {
@@ -16,9 +18,9 @@ TEST(AutoDiffTest, SubOpNodeTest) {
   auto b = std::make_shared<IndVarNode>(2.0);
   auto a_b_sub = a - b;
   a_b_sub->prop(1.0);
-  EXPECT_EQ(a_b_sub->value, -1.0);
-  EXPECT_EQ(a->grad, 1.0);
-  EXPECT_EQ(b->grad, -1.0);
+  EXPECT_NEAR(a_b_sub->value, -1.0, 1e-10);
+  EXPECT_NEAR(a->grad, 1.0, 1e-10);
+  EXPECT_NEAR(b->grad, -1.0, 1e-10);
 }
 
 TEST(AutoDiffTest, MulOpNodeTest) {
@@ -26,9 +28,9 @@ TEST(AutoDiffTest, MulOpNodeTest) {
   auto b = std::make_shared<IndVarNode>(2.0);
   auto a_b_mul = a * b;
   a_b_mul->prop(1.0);
-  EXPECT_EQ(a_b_mul->value, 2.0);
-  EXPECT_EQ(a->grad, 2.0);
-  EXPECT_EQ(b->grad, 1.0);
+  EXPECT_NEAR(a_b_mul->value, 2.0, 1e-10);
+  EXPECT_NEAR(a->grad, 2.0, 1e-10);
+  EXPECT_NEAR(b->grad, 1.0, 1e-10);
 }
 
 TEST(AutoDiffTest, DivOpNodeTest) {
@@ -36,9 +38,9 @@ TEST(AutoDiffTest, DivOpNodeTest) {
   auto b = std::make_shared<IndVarNode>(2.0);
   auto a_b_div = a / b;
   a_b_div->prop(1.0);
-  EXPECT_EQ(a_b_div->value, 0.5);
-  EXPECT_EQ(a->grad, 0.5);
-  EXPECT_EQ(b->grad, -0.25);
+  EXPECT_NEAR(a_b_div->value, 0.5, 1e-10);
+  EXPECT_NEAR(a->grad, 0.5, 1e-10);
+  EXPECT_NEAR(b->grad, -0.25, 1e-10);
 }
 
 
@@ -47,30 +49,93 @@ TEST(AutoDiffTest, ConstantNodeTest) {
   double b = 2.0;
   auto a_b_add = a + b;
   a_b_add->prop(1.0);
-  EXPECT_EQ(a_b_add->value, 3.0);
-  EXPECT_EQ(a->grad, 1.0);
+  EXPECT_NEAR(a_b_add->value, 3.0, 1e-10);
+  EXPECT_NEAR(a->grad, 1.0, 1e-10);
   a->grad = 0.0;
 
   auto a_b_sub = a - b;
   a_b_sub->prop(1.0);
-  EXPECT_EQ(a_b_sub->value, -1.0);
-  EXPECT_EQ(a->grad, 1.0);
+  EXPECT_NEAR(a_b_sub->value, -1.0, 1e-10);
+  EXPECT_NEAR(a->grad, 1.0, 1e-10);
   a->grad = 0.0;
 
   auto a_b_mul = a * b;
   a_b_mul->prop(1.0);
-  EXPECT_EQ(a_b_mul->value, 2.0);
-  EXPECT_EQ(a->grad, 2.0);
+  EXPECT_NEAR(a_b_mul->value, 2.0, 1e-10);
+  EXPECT_NEAR(a->grad, 2.0, 1e-10);
   a->grad = 0.0;
 
   auto a_b_div = a / b;
   a_b_div->prop(1.0);
-  EXPECT_EQ(a_b_div->value, 0.5);
-  EXPECT_EQ(a->grad, 0.5);
+  EXPECT_NEAR(a_b_div->value, 0.5, 1e-10);
+  EXPECT_NEAR(a->grad, 0.5, 1e-10);
   a->grad = 0.0;
-
 }
 
+
+TEST(AutoDiffTest, NegOpNodeTest) {
+  auto a = std::make_shared<IndVarNode>(3.0);
+  auto a_b_neg = -a;
+  a_b_neg->prop(1.0);
+  EXPECT_NEAR(a_b_neg->value, -3.0, 1e-10);
+  EXPECT_NEAR(a->grad, -1.0, 1e-10);
+}
+
+TEST(AutoDiffTest, SinOpNodeTest) {
+  auto a = std::make_shared<IndVarNode>(M_PI);
+  auto sin_a = sin(a);
+  sin_a->prop(1.0);
+  EXPECT_NEAR(sin_a->value, 0, 1e-10);
+  EXPECT_NEAR(a->grad, -1.0, 1e-10);
+}
+
+TEST(AutoDiffTest, CosOpNodeTest) {
+  auto a = std::make_shared<IndVarNode>(M_PI);
+  auto cos_a = cos(a);
+  cos_a->prop(1.0);
+  EXPECT_NEAR(cos_a->value, -1.0, 1e-10);
+  EXPECT_NEAR(a->grad, 0, 1e-10);
+}
+
+TEST(AutoDiffTest, TanOpNodeTest) {
+  auto a = std::make_shared<IndVarNode>(M_PI);
+  auto tan_a = tan(a);
+  tan_a->prop(1.0);
+  EXPECT_NEAR(tan_a->value, 0, 1e-10);
+  EXPECT_NEAR(a->grad, 1.0, 1e-10);
+}
+
+TEST(AutoDiffTest, ExpOpNodeTest) {
+  auto a = std::make_shared<IndVarNode>(std::log(5.0));
+  auto exp_a = exp(a);
+  exp_a->prop(1.0);
+  EXPECT_NEAR(exp_a->value, 5.0, 1e-10);
+  EXPECT_NEAR(a->grad, 5.0, 1e-10);
+}
+
+TEST(AutoDiffTest, LogOpNodeTest) {
+  auto a = std::make_shared<IndVarNode>(std::exp(5.0));
+  auto log_a = log(a);
+  log_a->prop(1.0);
+  EXPECT_NEAR(log_a->value, 5.0, 1e-10);
+  EXPECT_NEAR(a->grad, 1 / std::exp(5.0), 1e-10);
+}
+
+TEST(AutoDiffTest, SqrtOpNodeTest) {
+  auto a = std::make_shared<IndVarNode>(100.0);
+  auto sqrt_a = sqrt(a);
+  sqrt_a->prop(1.0);
+  EXPECT_NEAR(sqrt_a->value, 10.0, 1e-10);
+  EXPECT_NEAR(a->grad, 0.05, 1e-10);
+}
+
+TEST(AutoDiffTest, AbsOpNodeTest) {
+  auto a = std::make_shared<IndVarNode>(-5.0);
+  auto abs_a = abs(a);
+  abs_a->prop(1.0);
+  EXPECT_NEAR(abs_a->value, 5.0, 1e-10);
+  EXPECT_NEAR(a->grad, -1.0, 1e-10);
+}
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
